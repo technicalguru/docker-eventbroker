20789f515bb7eeed21e6fdb49f2cf359eaf665f8
ralph <github@ralph-schuster.eu>
2018-06-17 19:42:35 +0200

    Load dbconfig location from env var

--- src/main/java/rs/eventbroker/db/EnvironmentUrlProvider.java
+++ src/main/java/rs/eventbroker/db/EnvironmentUrlProvider.java
@@ -22,7 +22,7 @@ public class EnvironmentUrlProvider implements IUrlProvider {
 
 	public static final IUrlProvider INSTANCE = new Selector();
 	public static final String DEFAULT_ENV = "DEFAULT";
-		
+
 	/**
 	 * Constructor.
 	 */
@@ -39,33 +39,42 @@ public class EnvironmentUrlProvider implements IUrlProvider {
 
 	/** Internal static selector class so we only lookup the config file once. */
 	private static class Selector implements IUrlProvider {
-		
+
 		private Map<String, String> envFiles = new HashMap<String, String>();
-		
+
 		private URL selectedFile = null;
-		
+
 		private Selector() {
-			envFiles.put(Environment.PROD.name(), "dbconfig-prod.xml");
+			envFiles.put(Environment.PROD.name(), "dbconfig.xml");
 			envFiles.put(Environment.TEST.name(), "dbconfig-test.xml");
 			envFiles.put(DEFAULT_ENV, "dbconfig.xml");
 		}
-		
+
 		/**
 		 * {@inheritDoc}
 		 */
 		@Override
 		public URL getUrl() {
 			if (selectedFile == null) {
-				String env = null;
+				String rc = null;
 				try {
-					env = System.getenv("EB_RUNTIME_ENVIRONMENT");
+					rc = System.getenv("EB_DB_CONFIG");
 				} catch (SecurityException e) {
 					// Ignore, we don't have permission to see this
 				}
-				// Default is production
-				if (env == null) env = DEFAULT_ENV;
-				LoggerFactory.getLogger(Environment.class).info("Environment set to "+env);
-				selectedFile = FileFinder.find(envFiles.get(env));
+				if (rc == null) {
+					String env = null;
+					try {
+						env = System.getenv("EB_RUNTIME_ENVIRONMENT");
+					} catch (SecurityException e) {
+						// Ignore, we don't have permission to see this
+					}
+					// Default is production
+					if (env == null) env = DEFAULT_ENV;
+					LoggerFactory.getLogger(Environment.class).info("Environment set to "+env);
+					rc = envFiles.get(env);
+				}
+				selectedFile = FileFinder.find(rc);
 				LoggerFactory.getLogger(EBDaoFactory.class).info("Config file: "+selectedFile.toString());
 			}
 			return selectedFile;
